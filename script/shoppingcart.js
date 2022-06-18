5b4275e95acb4aed17efaaa26113b7e934ac7d06
Yuki <kingbavuong2002@gmail.com>
2022-06-18 22:34:02 +0700

    Finish shopping cart script

--- script/shoppingcart.js
+++ script/shoppingcart.js
@@ -4,23 +4,6 @@ var btn = document.getElementById("cart");
 var close = document.getElementsByClassName("close")[0];
 var close_footer = document.getElementsByClassName("close-footer")[0];
 var order = document.getElementsByClassName("order")[0];
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-close.onclick = function () {
-  modal.style.display = "none";
-}
-close_footer.onclick = function () {
-  modal.style.display = "none";
-}
-order.onclick = function () {
-  alert("Cảm ơn bạn đã thanh toán đơn hàng")
-}
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 // delete cart
 var remove_cart = document.getElementsByClassName("btn-danger");
@@ -115,3 +98,26 @@ function addItemToCart(title, price) {
   })
 }
 
+/* Modal click*/
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+close.onclick = function () {
+  modal.style.display = "none";
+}
+close_footer.onclick = function () {
+  modal.style.display = "none";
+}
+order.onclick = function () {
+  if (document.getElementsByClassName("cart-total-price")[0].innerHTML == '0VNĐ'){
+    alert("Giỏ hàng trống!");
+  }
+  else {
+    window.location="payment.html";
+  }
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
