0184916d6b0d77ae3196ea82ba130fb5306e1784
Yuki <kingbavuong2002@gmail.com>
2022-06-18 18:41:59 +0700

    fix price bug

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ function updatecart() {
     var price_item = cart_row.getElementsByClassName("cart-price ")[0]
     var quantity_item = cart_row.getElementsByClassName("cart-quantity-input")[0]
     var price = parseFloat(price_item.innerText)// transform string into number to calc total.
-    var quantity = quantity_item.value // get value in input tag
+    var quantity = quantity_item.value // get value from input tag
     total = total + (price * quantity)
   }
   document.getElementsByClassName("cart-total-price")[0].innerText = total + 'VNĐ'
@@ -78,7 +78,7 @@ for (var i = 0; i < add_cart.length; i++) {
   })
 }
 
-function addItemToCart(title, price, img) {
+function addItemToCart(title, price) {
   var cartRow = document.createElement('div')
   cartRow.classList.add('cart-row')
   var cartItems = document.getElementsByClassName('cart-items')[0]
@@ -114,36 +114,4 @@ function addItemToCart(title, price, img) {
     updatecart()
   })
 }
-// Modal
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("cart");
-var close = document.getElementsByClassName("close")[0];
-var close_footer = document.getElementsByClassName("close-footer")[0];
-var order = document.getElementsByClassName("order")[0];
-btn.onclick = function () {
-  modal.style.display = "block";
-}
-close.onclick = function () {
-  modal.style.display = "none";
-}
-close_footer.onclick = function () {
-  modal.style.display = "none";
-}
-order.onclick = function () {
-  alert("Cảm ơn bạn đã thanh toán đơn hàng")
-}
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
-// delete cart
-var remove_cart = document.getElementsByClassName("btn-danger");
-for (var i = 0; i < remove_cart.length; i++) {
-  var button = remove_cart[i]
-  button.addEventListener("click", function () {
-    var button_remove = event.target
-    button_remove.parentElement.parentElement.remove()
-  })
-}
